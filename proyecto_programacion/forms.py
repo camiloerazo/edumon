dcb9a09c5eb6b9d4499bbf9317714c5ee753d24f
camiloerazo <38138409+camiloerazo@users.noreply.github.com>
2022-01-24 14:59:21 -0500

    Delete proyecto_programacion directory

--- proyecto_programacion/forms.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form
-from wtforms import StringField, TextField, PasswordField
-from wtforms.fields.html5 import EmailField
-
-
-class CommentForm(Form):
-    email = EmailField('email')
-    password = PasswordField('password')
-    
